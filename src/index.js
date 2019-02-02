51913d43328e2ca39c2301a147966318c553f0d2
spartanhero93 <spartanhero93@gmail.com>
2019-02-02 11:49:08 -0500

    added plugins for babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-window.onload(() => alert('Hello Waifus'))
+import axios from 'axios'
+
+class WeatherApp {
+  // constructor(city, countryCode = 'us') {
+  //   this.city = city
+  //   this.countryCode = countryCode
+  //   this.key = 'e00dcf67e1a09b99b9ddbd2aa28441df'
+  // }
+
+  constructor() {
+    this.input = document.getElementById('input')
+  }
+
+  init() {
+    input.addEventListener('input', event => console.log(event.target.value))
+  }
+
+  fetchWeather = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${this.city},${
+          this.countryCode
+        }&mode=json&APPID=${this.key}`
+      )
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+const Wa = new WeatherApp()
+Wa.init()
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,65 @@
 const path = require('path')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    library: 'UserList',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
+
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+const APP_DIR = path.resolve(__dirname, 'src')
+
+module.exports = env => {
+  const { PLATFORM, VERSION } = process.env
+  return merge([
+    {
+      entry: ['@babel/polyfill', APP_DIR],
+      output: {
+        path: BUILD_DIR,
+        filename: '[name].js',
       },
-    ],
-  },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.(png|svg|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 25000,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: './index.html',
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(process.env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(process.env.PLATFORM),
+        }),
+        /** Static images variant:
+         * new CopyWebpackPlugin([{ from: 'src/static' }])
+         * */
+      ],
+    },
+  ])
 }
